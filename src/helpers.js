3ff700d7bc4c2cf883bc88c1ddde5800d1ce1d32
Vera Deeva <verasdee@gmail.com>
2024-10-18 13:15:56 +0700

    add comments to helper

--- src/helpers.js
+++ src/helpers.js
@@ -12,3 +12,21 @@ export const reformateDate = (dateStr) => {
     const month = months[monthN];
     return (month)
 };
+
+// 0° — north wind (N)
+// 22.5° — north-northeast wind(NNE)
+// 45° — northeast wind(NE)
+// 67.5° — east-northeast wind(ENE)
+// 90°— east wind(E)
+// 112.5° — east-southeast wind(ESE)
+// 135° — southeast wind(SE)
+// 157.5° — south-southeast wind(SSE)
+// 180° — south wind(S)
+// 202.5° — south-southwest wind(SSW)
+// 225° — southwest wind(SW)
+// 247.5° — west-southwest wind(WSW)
+// 270° — west wind(W)
+// 292.5° — west-northwest wind(WNW)
+// 315° — northwest wind(NW)
+// 337.5° — north-northwest wind(NNW)
+// 360° — north wind(N)
