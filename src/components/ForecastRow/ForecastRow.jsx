b7a7db276d2420b25e8e51aea5fd3521ef45e63a
Vera Deeva <verasdee@gmail.com>
2024-10-18 21:32:16 +0700

    add function for make wind_dir as arrow

--- src/components/ForecastRow/ForecastRow.jsx
+++ src/components/ForecastRow/ForecastRow.jsx
@@ -1,3 +1,4 @@
+import { drawDirectionWind } from '../../helpers';
 import { Loading } from '../Loading/Loading';
 import s from './ForecastRow.module.css';
 
@@ -15,7 +16,8 @@ export const ForecastRow = ({ data, j }) => {
         </td>
         <td className={`${s.item} ${s.text}`}>{data.condition.text}</td>     
         <td className={`${s.item} ${s.wind}`}>{data.wind_kph} km/h</td>
-        <td className={`${s.item} ${s.wind_dir}`}>{data.wind_dir}</td>     
+            <td className={`${s.item} ${s.wind_dir}`} style={{ transform: `rotate(${drawDirectionWind(data.wind_dir)}deg)`}}>&#8595;</td> 
+        {/* <td className={`${s.item} ${s.wind_dir}`}>{drawDirectionWind(data.wind_dir)}</td>      */}
         <td className={`${s.item} ${s.feel}`}>{data.feelslike_c}&#176;C</td>
     </tr>
 )}
