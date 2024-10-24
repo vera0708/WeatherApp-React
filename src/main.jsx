441728f0ad80bd1e87e56e0db565c023ea18bc6a
Vera Deeva <verasdee@gmail.com>
2024-10-24 15:28:26 +0700

    implement persist redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import 'normalize.css'
-import './index.css'
-import { Provider} from 'react-redux'
-import { store } from './store/store.js'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import 'normalize.css';
+import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import  store, { persistor } from './store/store.js';
+import { Loading } from './components/Loading/Loading.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<Loading/>} persistor={persistor}>
+      <App />
+    </PersistGate>    
   </Provider>,
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import { createStore } from 'redux';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
 import authReducer from "./auth/auth.slice";
 import weatherReducer from "./weather/weather.slice";
 import favoriteReducer from "./favorite/favorite.slice";
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        weather: weatherReducer,
-        favorite: favoriteReducer,
-    },
-})
+const rootReducer = combineReducers({
+    auth: authReducer,
+    weather: weatherReducer,
+    favorite: favoriteReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+ 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
